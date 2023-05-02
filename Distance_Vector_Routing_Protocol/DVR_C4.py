23981678ab7cbdc4dc7eca8f04a49366cb655194
RFREM <64663164+RFREM@users.noreply.github.com>
2023-05-02 00:43:30 -0700

    Adding 4 instances to run

---
+++ Distance_Vector_Routing_Protocol/DVR_C4.py
@@ -0,0 +1,8 @@
+
+import socket
+import threading
+
+#computer name of client
+hostname=socket.gethostname() 
+#ipaddress of clinet 
+IPAddrGET=socket.gethostbyname(hostname) 
--- Distance_Vector_Routing_Protocol/Distance_Vector_Routing_Protocol.py
+++ Distance_Vector_Routing_Protocol/Distance_Vector_Routing_Protocol.py
@@ -1,1 +1,11 @@
+import socket
+import threading
+import subprocess
 
+subprocess.call('start DVR_C1.py', shell=True)
+subprocess.call('start DVR_C2.py', shell=True)
+subprocess.call('start DVR_C3.py', shell=True)
+subprocess.call('start DVR_C4.py', shell=True)
+
+hostname=socket.gethostname()   
+IPAddrGET=socket.gethostbyname(hostname) 
